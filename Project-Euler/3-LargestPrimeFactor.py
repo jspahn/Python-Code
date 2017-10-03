2aafcb0e609bb631d276073010957cd6eb67fdb6
Jeffrey Spahn <jspahn.04@gmail.com>
2017-10-03 17:54:43 -0400

    Finished Problem 2 and started Problem 3

---
+++ Project-Euler/3-LargestPrimeFactor.py
@@ -0,0 +1,12 @@
+# Project Euler
+# Problem 3: Largest Prime Factor
+#       The prime factors of 13195 are 5, 7, 13 and 29.
+#       What is the largest prime factor of the number 600851475143?
+
+
+# Jeffrey Spahn
+# created for Python 3.x
+
+n = 600851475143
+
+def findNextPrime(lPrimes):
