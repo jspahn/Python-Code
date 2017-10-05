c697a22fd8fda26ba30d4a02d10e7b1b22a00e4e
Jeff Spahn <jspahn.04@gmail.com>
2017-10-04 23:54:01 -0400

    Prob4 - finished isPalindrome Function

--- Project-Euler/4-PalindromeProject.py
+++ Project-Euler/4-PalindromeProject.py
@@ -22,3 +22,24 @@ def isPalindrome(n):
         if s[i] != s[-i-1]:
             b_isP = False
     return b_isP
+
+# findLargest_inRange(low, high)
+#       searches for two natural numbers between the low and the high
+#       that when multiplied give a palindrome.
+#       returns a tuple of those two numbers
+def findLargest_inRange(low_i,high_i, low_j, high_j):
+    result = (-1,-1)
+    for i in range(low_i,high_i+1):
+        for j in range(low_j,high_j+1):
+            if isPalindrome(i*j) and i*j > result[0]*result[1]:
+                result = (i,j)
+    return result
+
+# Brute Force method:
+solution = findLargest_inRange(100,999, 100, 999)
+print(str(solution))
+print(str(solution[0]*solution[1]))
+
+# Output:
+#       (913, 993)
+#       906609
