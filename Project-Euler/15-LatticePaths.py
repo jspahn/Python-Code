0fb05376b0ddbbe88822ab3ea65b780674c1f38e
Jeff Spahn <jspahn.04@gmail.com>
2017-10-06 16:15:38 -0400

    Prob15 - Wrote out Problem 15

---
+++ Project-Euler/15-LatticePaths.py
@@ -0,0 +1,21 @@
+# Project Euler
+# Problem 15: Lattice Paths
+#       Starting in the top left corner of a 2x2 grid, and only being able
+#       to move to the right and down, there are exactly 6 routes to the
+#       bottom right corner.
+#            * *        * _        * _
+#           |_|_*      |_*_|      |_**|
+#           |_|_*      |_**|      |_|_*
+#           RRDD        RDDR       RDRD
+#
+#            _ _        _ _        _ _
+#           **|*|      **|_|      *_|_|
+#           |_|_*      |_**|      **|*|
+#           DRRD       DRDR       DDRR
+#
+#       How many such routes are there through a 20x20 grid?
+
+# Jeffrey Spahn
+# Created for Python 3.x
+
+# TODO Complete Problem 15
