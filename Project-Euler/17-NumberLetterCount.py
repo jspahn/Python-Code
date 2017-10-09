a91800d26102f2a275a3594bf276e7029b5cf1ec
Jeffrey Spahn <jspahn.04@gmail.com>
2017-10-09 15:27:20 -0400

    Prob17 - Completed

--- Project-Euler/17-NumberLetterCount.py
+++ Project-Euler/17-NumberLetterCount.py
@@ -13,36 +13,75 @@
 # Jeffrey Spahn
 # Created for Python 3.x
 
-# TODO Complete Problem 17
+import time
 
-'''
-one
-two
-three
-four
-five
-six
-seven
-eight
-nine
-ten
-eleven
-twelve
-thirteen
-        fourteen
-fifteen
-        sixteen
-        seventeen
-        eighteen
-        nineteen
-twenty
-thirty
-forty
-fifty
-        sixty
-        seventy
-        eighty
-        ninety
-hundred
-thousand
-'''
+startTime = time.time()
+
+digitLengths = {
+    1:   3,  # one
+    2:   3,  # two
+    3:   5,  # three
+    4:   4,  # four
+    5:   4,  # five
+    6:   3,  # six
+    7:   5,  # seven
+    8:   5,  # eight
+    9:   4,  # nine
+    10:  3,  # ten
+    11:  6,  # eleven
+    12:  6,  # twelve
+    13:  8,  # thirteen
+    14:  8,  # fourteen
+    15:  7,  # fifteen
+    16:  7,  # sixteen
+    17:  9,  # seventeen
+    18:  8,  # eighteen
+    19:  8,  # nineteen
+    20:  6,  # twenty
+    30:  6,  # thirty
+    40:  5,  # forty
+    50:  5,  # fifty
+    60:  5,  # sixty
+    70:  7,  # seventy
+    80:  6,  # eighty
+    90:  6,  # ninety
+    100: 7,  # hundred
+    1000:8   # thousand
+}
+
+
+def getLetterCount(i):
+    letter_count = 0
+
+    if i // 1000 != 0:
+        letter_count += digitLengths[i // 1000]  # one, two, three...
+        letter_count += digitLengths[1000]       # thousand
+    if (i%1000) // 100  != 0:
+        letter_count += digitLengths[(i%1000) // 100] # one, two, three...
+        letter_count += digitLengths[100]             # hundred
+        if i % 100  != 0:
+            letter_count += 3                         # and
+    if (i%100) // 10   >1:
+        letter_count += digitLengths[((i%100) // 10) * 10]   # twenty, thirty, forty...
+
+    if (i % 100) // 10 == 1:
+        letter_count += digitLengths[i%100]   # ten, eleven, twelve, ... nineteen
+    elif i % 10 != 0:
+        letter_count += digitLengths[i % 10]  # one, two, three...
+
+    return letter_count
+
+
+total_letter_count = 0
+
+
+
+for i in range(1001):
+    total_letter_count += getLetterCount(i)
+
+print("The total letter count of all numbers between 1 and 1000 (inclusive) is: {}".format(total_letter_count))
+print("Time: {}".format(time.time() - startTime))
+
+# Output
+#     The total letter count of all numbers between 1 and 1000 (inclusive) is: 21124
+#     Time: 0.0011241436004638672
