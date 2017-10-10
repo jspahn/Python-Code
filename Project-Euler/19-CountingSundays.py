967386faaadc00a9b74a92e8092fbf15ac0e0912
Jeff Spahn <jspahn.04@gmail.com>
2017-10-10 10:23:02 -0400

    Prob19 - Completed

--- Project-Euler/19-CountingSundays.py
+++ Project-Euler/19-CountingSundays.py
@@ -26,6 +26,9 @@ Notes:
           Feb starts 1 + 31%7
 
 '''
+import time
+
+start_time = time.time()
 
 month_length = {
     1 : 31,  # January
@@ -41,3 +44,35 @@ month_length = {
     11: 30,  # November
     12: 31   # December
 }
+
+def isLeapYear(n):
+    if n % 400 == 0:
+        return True
+    elif n % 100 == 0:
+        return False
+    elif n % 4 == 0:
+        return True
+    else:
+        return False
+
+sunday_count = 0
+day_index = 1       # Sunday = 0 , Monday = 1, ... Saturday = 6
+for year in range(0,101):
+    for month in range(1,13):
+        if not (year == 100 and month == 12):
+            day_index = day_index + month_length[month]
+            if month == 2 and isLeapYear(1900 + year):
+                day_index = day_index + 1
+            day_index = day_index % 7
+            if day_index == 0 and year > 0:
+                    sunday_count += 1
+
+
+
+print("The total number of months in the 20th Century that started on a Sunday are {}".format(sunday_count))
+print("Completion time: {}".format(time.time()-start_time))
+
+
+# Output
+#     The total number of months in the 20th Century that started on a Sunday are 171
+#     Completion time: 0.001010894775390625
