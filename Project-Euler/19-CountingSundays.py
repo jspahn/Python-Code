609fec098b67e0f9fc1df077a301697cbcfa9822
Jeffrey Spahn <jspahn.04@gmail.com>
2017-10-09 21:44:31 -0400

    Prob19 - Added Dictionary for Month Lengths

--- Project-Euler/19-CountingSundays.py
+++ Project-Euler/19-CountingSundays.py
@@ -19,3 +19,25 @@
 
 # Jeffrey Spahn
 # Created for Python 3.x
+
+'''
+Notes:
+    1900: Jan starts Mon (1)
+          Feb starts 1 + 31%7
+
+'''
+
+month_length = {
+    1 : 31,  # January
+    2 : 28,  # February - Non Leap Year
+    3 : 31,  # March
+    4 : 30,  # April
+    5 : 31,  # May
+    6 : 30,  # June
+    7 : 31,  # July
+    8 : 31,  # August
+    9 : 30,  # September
+    10: 31,  # October
+    11: 30,  # November
+    12: 31   # December
+}
