f95f65e156ebe97b49896c690980c3a853d680e7
Jeffrey Spahn <jspahn.04@gmail.com>
2017-10-13 16:29:20 -0400

    Prob1 - Organized code into methods

--- Project-Euler/01-Multiples.py
+++ Project-Euler/01-Multiples.py
@@ -8,13 +8,27 @@
 
 # Jeffrey Spahn
 # created for Python 3.x
-
-result = 0
-for x in range(1,1000):
-    if x%3 == 0 or x%5 == 0:
-        result += x
+import time
 
 
-print("The sum of all multiples of 3 or 5 below 1000 is " + str(result))
+def sum_of_multiples(n, divisors):
+    """Returns the sum of all natural numbers below n that are divisible by divisors"""
+    result = 0
+    for x in range(1, n):
+        if any([x % div == 0 for div in divisors]):
+            result += x
+    return result
 
-# Solution calculated is 233168
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    print("The sum of all multiples of 3 or 5 below 1000 is {}".format(sum_of_multiples(1000,[3,5])))
+    print("Completion time: {}".format(time.time() - start_time))
+    # Output:
+    #      The sum of all multiples of 3 or 5 below 1000 is 233168
+    #      Completion time: 0.0006198883056640625
+
+
+
+
