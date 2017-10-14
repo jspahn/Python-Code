65ea65bd5b9fbca6afd4385921383a5229d2f0a0
Jeff Spahn <jspahn.04@gmail.com>
2017-10-14 04:01:36 -0400

    Finished

--- Project-Euler/24-LexicographicPermutations.py
+++ Project-Euler/24-LexicographicPermutations.py
@@ -10,8 +10,6 @@
 #       What is the millionth lexicographic permutation of the digits
 #       0, 1, 2, 3, 4, 5, 6, 7, 8, and 9?
 
-# Todo Submit Problem 24 Solution to Project Euler
-
 # Jeffrey Spahn
 # Created for Python 3.x
 
