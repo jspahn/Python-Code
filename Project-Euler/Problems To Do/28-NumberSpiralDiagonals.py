e7441e6fc312b7ad5d2f30e934fb8ce9c6397d8e
Jeffrey Spahn <jspahn.04@gmail.com>
2017-10-17 21:35:59 -0400

    Added Problem

---
+++ Project-Euler/Problems To Do/28-NumberSpiralDiagonals.py
@@ -0,0 +1,32 @@
+# Project Euler
+# Problem 28:  Number spiral diagonals
+#     Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+#
+#             *21*  22   23   24  *25*
+#              20  * 7*   8  * 9*  10
+#              19    6  * 1*   2   11
+#              18  * 5*   4  * 3*  12
+#             *17*  16   15   14  *13*
+#
+#     It can be verified that the sum of the numbers on the diagonals is 101.
+#
+#     What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral
+#       formed in the same way?
+
+
+# Jeffrey Spahn
+# Created for Python 3.x
+
+import time
+
+
+
+# ------------------------------------------------------------
+#  Main
+# ------------------------------------------------------------
+if __name__ == "__main__":
+    start_time = time.time()
+
+
+    print("Completion time: {}".format(time.time()-start_time))
+    # Output:
