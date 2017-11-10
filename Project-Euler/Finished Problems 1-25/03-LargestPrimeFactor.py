c11b34085b29f1c1f45f1c6fef094b4c1fe33e88
Jeffrey Spahn <jspahn.04@gmail.com>
2017-11-10 01:37:30 -0500

    Fixed some formatting

--- Project-Euler/Finished Problems 1-25/03-LargestPrimeFactor.py
+++ Project-Euler/Finished Problems 1-25/03-LargestPrimeFactor.py
@@ -9,47 +9,49 @@
 
 import time
 
-def findNextPrime(lPrimes):
+
+def find_next_prime(l_primes):
     """Looks for the next prime number to be added to the list
         Returns lPrimes with new prime appended to the end"""
 
-    i = lPrimes[-1]+2
-    isPrime = False
-    while isPrime == False:
-        isPrime = True
-        for p in lPrimes:
+    i = l_primes[-1]+2
+    is_prime = False
+    while not is_prime:
+        is_prime = True
+        for p in l_primes:
             if i % p == 0:
-                isPrime = False
+                is_prime = False
                 i += 2
                 break
-    lPrimes.append(i)
-    return lPrimes
+    l_primes.append(i)
+    return l_primes
 
-def findLargestPrimeFactor(n):
+
+def find_largest_prime_factor(n):
     """Finds the largest Prime Factor of n"""
 
-    lPrimes = [2, 3]  # List of prime numbers
+    l_primes = [2, 3]  # List of prime numbers
     while n % 2 == 0:
         n /= 2
     while n % 3 == 0:
         n /= 2
 
     while n != 1:
-        lPrimes = findNextPrime(lPrimes)
-        while n % lPrimes[-1] == 0:
-            n /= lPrimes[-1]
+        l_primes = find_next_prime(l_primes)
+        while n % l_primes[-1] == 0:
+            n /= l_primes[-1]
 
-    return lPrimes[-1]
+    return l_primes[-1]
 
-#------------------------------------------------------------
+
+# ------------------------------------------------------------
 #  Main
-#------------------------------------------------------------
+# ------------------------------------------------------------
 if __name__ == "__main__":
     start_time = time.time()
     testNumber = 600851475143
 
-
-    solution = findLargestPrimeFactor(testNumber)
+    solution = find_largest_prime_factor(testNumber)
 
     print("The largest Prime Factor of {} is {}".format(testNumber,solution))
     print("Completion time: {}".format(time.time() - start_time))
