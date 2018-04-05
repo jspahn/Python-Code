cb5aa09185203f48a8ccbb75927c35e35e0d9efc
Jeffrey Spahn <jspahn.04@gmail.com>
2018-04-04 19:50:44 -0700

    Completed

--- Project-Euler/Problems To Do/31-CoinSums.py
+++ Project-Euler/Problems To Do/31-CoinSums.py
@@ -15,14 +15,68 @@
 
 import time
 
+
+def num_ways(coin_denominations, target_value):
+    """Finds all combinations of coins that add up to the target value."""
+    combination_count = 0
+    coin_count = {c : 0 for c in coin_denominations}
+    l_combinations = []
+    coin_denominations.sort()
+    coin_denominations.reverse()
+
+    largest_coin = coin_denominations[0]
+    delta_coin = coin_denominations[0] + 1
+
+    # If the last combination was not made of all the smallest denominations, then look for more combinations
+    while largest_coin > coin_denominations[-1]:
+        value = 0
+
+        if delta_coin == coin_denominations[-1]:
+            delta_coin = coin_denominations[-2]
+            while coin_count[delta_coin] == 0 and coin_denominations.index(delta_coin) > 0:
+                delta_coin = coin_denominations[coin_denominations.index(delta_coin) - 1]
+
+        for c in coin_denominations:
+            if c == delta_coin:
+                coin_count[c] = coin_count[c] - 1
+            if c < delta_coin:
+                coin_count[c] = (target_value - value) // c
+            value += coin_count[c] * c
+
+        if value == target_value:
+            combination_count += 1
+            delta_coin = c
+            l_combinations.append(list(coin_count.values()))
+
+        largest_coin = 0
+        # find largest coin
+        for c in coin_denominations:
+            if coin_count[c] > 0:
+                largest_coin = c
+                break
+    return l_combinations
+
+
 # ------------------------------------------------------------
 #  Main
 # ------------------------------------------------------------
 if __name__ == "__main__":
     start_time = time.time()
 
+    coin_denominations = [100, 50, 25, 10, 5, 1]
+    goal_value = 200
 
+    all_combinations = num_ways(coin_denominations, goal_value)
+    for combin in all_combinations:
+        print(combin)
 
+    solution = len(all_combinations)
+
+    print("\nThe Answer is: {}".format(solution))
     print("Completion time: {}".format(time.time() - start_time))
 
     # Output:
+    #     The Answer is: 73682
+    #     Completion time: 0.19931387901306152
+
+
--- Project-Euler/Problems To Do/32-PandigitalProducts.py
+++ Project-Euler/Problems To Do/32-PandigitalProducts.py
@@ -17,6 +17,28 @@
 # Created for Python 3.x
 
 import time
+import math
+
+
+def num_of_digits(test_number):
+    return int(math.log(test_number,10))+1
+
+
+def is_pandigital(test_numbers, digit_length):
+    total_num_digits = 0
+    test = []
+    for tn in test_numbers:
+        total_num_digits += num_of_digits(tn)
+        test.extend([int(x) for x in str(tn)])
+    if total_num_digits != digit_length:
+        return False
+
+    test.sort()
+    digits = list(range(1,digit_length+1))
+    if test == digits:
+        return True
+    return False
+
 
 # ------------------------------------------------------------
 #  Main
@@ -24,6 +46,25 @@ import time
 if __name__ == "__main__":
     start_time = time.time()
 
+    l_poss_sol = []
+    for a in range(1,9999+1):
+        for b in range(1,pow(10,5 - num_of_digits(a))):
+            ab = a * b
+            tot_num_digits = num_of_digits(a) + num_of_digits(b) + num_of_digits(ab)
+            if tot_num_digits > 9:
+                break
+            if tot_num_digits == 9:
+                l_poss_sol.append([a, b, ab])
+
+    l_products = []
+    for poss in l_poss_sol:
+        if is_pandigital(poss,9):
+            l_products.append(poss[2])
+    l_products = list(set(l_products))
+
+    print("The Sum of all the products is {}".format(sum(l_products)))
     print("Completion time: {}".format(time.time() - start_time))
 
     # Output:
+    #       The Sum of all the products is 45228
+    #       Completion time: 0.7163009643554688
